2da084629b7aaa18654be6a39d00178ae721e46b
JMaylor <60825814+JMaylor@users.noreply.github.com>
2023-05-07 15:50:54 +0100

    currency combobox

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import VListbox from './components/VListbox.vue'
-import { AcademicCapIcon } from '@heroicons/vue/20/solid'
+import { AcademicCapIcon, CurrencyDollarIcon, CurrencyEuroIcon, CurrencyPoundIcon, CurrencyYenIcon } from '@heroicons/vue/20/solid'
 import VCombobox from './components/VCombobox.vue';
 
 const people = ref([
@@ -24,8 +24,31 @@ const people = ref([
     iconBackground: 'bg-rose-500'
   },
 ])
-
 const person = ref<number>()
+
+const currencies = ref([
+  {
+    iso_code: 'USD',
+    name: 'US Dollar',
+    icon: CurrencyDollarIcon
+  },
+  {
+    iso_code: 'EUR',
+    name: 'Euro',
+    icon: CurrencyEuroIcon
+  },
+  {
+    iso_code: 'GBP',
+    name: 'British Pound',
+    icon: CurrencyPoundIcon
+  },
+  {
+    iso_code: 'JPY',
+    name: 'Japanese Yen',
+    icon: CurrencyYenIcon
+  },
+])
+const currency = ref<string>()
 </script>
 
 <template>
@@ -47,29 +70,23 @@ const person = ref<number>()
           <component
             :is="item.icon"
             v-else
-            class="h-4 w-4"
+            class="h-5 w-5"
           />
         </template>
       </VListbox>
     </div>
     <div class="col-span-3">
       <VCombobox
-        v-model="person"
-        :items="people"
+        v-model="currency"
+        :items="currencies"
         label="Combobox"
-        value-key="id"
+        value-key="iso_code"
         display-key="name"
       >
         <template #icon="{item}">
-          <div
-            v-if="'iconBackground' in item"
-            class="h-4 w-4 rounded-full"
-            :class="item.iconBackground"
-          />
           <component
             :is="item.icon"
-            v-else
-            class="h-4 w-4"
+            class="h-5 w-5"
           />
         </template>
       </VCombobox>
