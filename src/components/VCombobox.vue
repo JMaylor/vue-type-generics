26998ea9d96086fab61250a8da6f2f49ffcc7221
JMaylor <60825814+JMaylor@users.noreply.github.com>
2023-05-07 11:03:31 +0100

    remove font sizes

--- src/components/VCombobox.vue
+++ src/components/VCombobox.vue
@@ -79,7 +79,7 @@ const filteredItems = computed(() =>
           class="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300"
         >
           <ComboboxInput
-            class="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+            class="w-full border-none py-2 pl-3 pr-10 leading-5 text-gray-900 focus:ring-0"
             :display-value="(item: unknown) => displayFn(getItemFromValue(item as TValue)!)"
             @change="query = $event.target.value"
           />
@@ -99,7 +99,7 @@ const filteredItems = computed(() =>
           @after-leave="query = ''"
         >
           <ComboboxOptions
-            class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
           >
             <div
               v-if="filteredItems.length === 0 && query !== ''"
